c4a47c30a9ffbc567dc8e5aab9da33eab7bc4952
Aunreddy15 <baddamarunreddy45@gmail.com>
2021-12-11 18:10:38 +0530

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,40 @@
 import { Component } from "react";
-import {FaLinkedinIn} from "react-icons/fa"
+import {BsGithub} from "react-icons/bs"
 import "./index.css"
 
+const apiStatus={
+    initial:"INITIAL",
+    success:"SUCCESS",
+    failure:"FAILURE",
+    in_Progress:"IN_PROGRESS"
+}
 class Home extends Component{
-state={username:"",loading:"",profile:[]}
+state={username:"Aunreddy15",api:apiStatus.initial,profile:[]}
+
+componentDidMount=async()=>{
+    const {username}=this.state
+    this.setState({api:apiStatus.in_Progress})
+    const response=await fetch(`https://api.github.com/users/${username}`)
+    const data=await response.json()
+    if (response.ok===true){
+        this.setState({profile:data,api:apiStatus.success})
+    }
+    else{
+        this.setState({api:apiStatus.failure})
+    }
+}
 
 onClickFind=async()=>{
     const {username}=this.state
-    this.setState({loading:"true"})
+    this.setState({api:apiStatus.in_Progress})
     const response=await fetch(`https://api.github.com/users/${username}`)
     const data=await response.json()
-if (response.ok===true){
-    this.setState({profile:data,loading:false})
-}
+    if (response.ok===true){
+        this.setState({profile:data,api:apiStatus.success})
+    }
+    else{
+        this.setState({api:apiStatus.failure})
+    }
 }
 
 onChangeUserName=(event)=>{
@@ -22,32 +44,65 @@ onChangeUserName=(event)=>{
 
 renderData=()=>{
     const {profile}=this.state
-    return <div className="profile">
-        <h1>{profile.login}</h1>
-        <img src={profile.avatar_url} alt="profile"/>
+    return <div className="profile-section">
+        <div className="profile-img-un">
+            <img className="profile-image" src={profile.avatar_url} alt="profile"/>
+            <div className="profile-data">
+                <h1>{profile.name}</h1>
+                <p>{profile.login}</p>
+            </div>
+            <button>Edit Profile</button>
+        </div>
+        <div></div>
     </div>
+}
+
+renderLoading=()=><div>Loading...</div>
+
+renderFail=()=>{
+    const{username}=this.state 
+    return<div className="error-fail"><p>{`There is no existing personal in Github with this username "${username}", Please try again with another name`}</p></div>
+}
+
+renderApiData=()=>{
+    const{api}=this.state
+    switch (api) {
+        case apiStatus.success:
+            
+           return this.renderData()
+        case apiStatus.failure:
+            
+            return this.renderFail()
+        case apiStatus.in_Progress:
+            
+                return this.renderLoading()    
+        default:
+            return null;
+    }
 }
     
 render(){
-        const {username,loading}=this.state
+        const {username}=this.state
         return<div className="home">
             <div className="nav-bar">
                     <h1>pro<span>.finder</span></h1>
-                    <div className="link-icon-container">
-                    <FaLinkedinIn size={26}></FaLinkedinIn></div>
+                    <div className="git-icon-container">
+                    <BsGithub size={32}></BsGithub></div>
             </div>
             <div className="home-inner-body">
             <div className="home-inner">
                 
                 <div className="body">
-                    <h1 className="wish">Hello, welcome!</h1>
-                    <div className="disclaimer">
-                        <p>to "Linkedin Profile Finder" </p>
-                        <input value={username} onChange={this.onChangeUserName} type="text" placeholder="Enter exact username of Linkedin user"></input>
+                    <div className="body-upper-section">
+                        <h1 className="wish">Hello, Welcome!</h1>
+                        <div className="disclaimer">
+                            <p>to "Github Profile Finder" </p>
+                            <input value={username} onChange={this.onChangeUserName} type="text" placeholder="Enter exact username of Github user"></input>
+                        </div>
+                        <button onClick={this.onClickFind} type="button" className="button-find">Find</button>
                     </div>
-                    <button onClick={this.onClickFind} type="button" className="button-find">Find</button>
                 <div>
-                    {loading?"Loading...":this.renderData()}
+                    {this.renderApiData()}
                 </div>
                 </div>
             </div>
